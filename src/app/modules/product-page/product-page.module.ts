296909b3663e130c36fed80c92ad3479e43e86cf
Juan Tavares <juantavares2006@hotmail.com>
2019-10-21 23:28:03 -0300

    Criando module de product-page

---
+++ src/app/modules/product-page/product-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductPageRoutingModule } from './product-page-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductPageRoutingModule
+  ]
+})
+export class ProductPageModule { }
